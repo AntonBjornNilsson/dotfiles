8fce0174ac13a2f9fdacc46bb9349a84bd429a6a
Anton Nilsson <dat15ani@student.lu.se>
2019-07-04 09:11:14 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,11 @@ cp .tmux.conf ~/.tmux.conf
 echo 'moved .tmux.conf'
 cp .vimrc ~/.vimrc
 echo 'moved .vimrc'
+cp .zshrc ~/.zshrc
+echo 'moved .zshrc'
+cp .gitignore ~/.gitignore
+echo 'moved .gitignore'
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo 'installed Vundle'
+
+echo 'Install complete, enjoy'
