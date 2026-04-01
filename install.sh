76c974d95e9b86d64ad1e2286e14a1bc03572996
AntonBjornNilsson <contact@antonbjornnilsson.com>
2026-04-01 22:12:01 +0200

    change k9s paths

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-all=(".tmux.conf" ".vimrc" ".zshrc" ".fzf.zsh" ".spaceshiprc.zsh")
+all=(".tmux.conf" ".vimrc" ".zshrc" ".fzf.zsh" ".spaceshiprc.zsh" ".config/k9s")
 for i in "${all[@]}" ; do
   rm -f $HOME/$i
+  [[ "$i" == *"/"* ]] && mkdir -p "$HOME/$(dirname "$i")"
   ln -s $(realpath $i) $HOME/$i
   echo "$i"
 done
-
+exit
 # apt
 xargs sudo apt install -y <apt-packages.txt
 
